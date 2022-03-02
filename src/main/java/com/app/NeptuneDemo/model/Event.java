a24e9d856df1ed43e9e3f525c5cba3ea6ac1dae4
mico0922 <98570242+MicoTumabiao@users.noreply.github.com>
2022-03-02 23:03:54 +0800

    Event HTML

--- src/main/java/com/app/NeptuneDemo/model/Event.java
+++ src/main/java/com/app/NeptuneDemo/model/Event.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "events")
+@Table(name = "event")
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
